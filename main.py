dd96ac2545687a832e25c2159e58e6200fad2f72
Aleksander Fidelus <alek.fidelus@gmail.com>
2020-10-23 21:48:41 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restful import Api, Resource
+import simplejson as json
+from SqlReader import SqlReader
+
+
+with open('db_config.json') as config_file:
+    conf_str = config_file.read()
+    db_config = json.loads(conf_str)
+
+app = Flask(__name__)
+api = Api(app)
+
+reader = SqlReader(db_config)
+
+
+@app.route('/doctors/', defaults={'specialty': 'all'})
+@app.route('/doctors/<string:specialty>',methods = ['GET'])
+def doctors_all_or_by_specialty(specialty):
+    result = reader.fetch(specialty)
+    return json.dumps(result, use_decimal=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
