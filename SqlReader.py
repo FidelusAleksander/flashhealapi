796bde5258a5c1f9dd4606021142f9716077c0ce
alekfidelus <alek.fidelus@gmail.com>
2020-10-24 15:55:03 +0200

    tryexcept

--- SqlReader.py
+++ SqlReader.py
@@ -1,73 +1,73 @@
-# import pyodbc
-#
-# class SqlReader:
-#     def __init__(self, config):
-#         self._config = config
-#         self.connstring = 'DRIVER=ODBC Driver 17 for SQL Server;SERVER={};DATABASE={};UID={};PWD={}'.format(
-#             self._config['sqlserver'],
-#             self._config['database'],
-#             self._config['user'],
-#             self._config['password'])
-#
-#     def connection(self):
-#         connection = pyodbc.connect(self.connstring)
-#         return connection
-#
-#     def fetch(self, specialty):
-#         if specialty != 'all':
-#             query = f"SELECT * FROM dbo.doctors WHERE specialty = '{specialty}'"
-#         else:
-#             query = "SELECT * FROM dbo.doctors"
-#
-#         cursor = None
-#         conn = None
-#
-#         result = []
-#
-#         try:
-#             conn = self.connection()
-#             cursor = conn.cursor()
-#             cursor.execute(query)
-#
-#             columns = [column[0] for column in cursor.description]
-#
-#             for row in cursor.fetchall():
-#                 result.append(dict(zip(columns, row)))
-#
-#         except Exception as e:
-#             raise e
-#         finally:
-#             if cursor:
-#                 cursor.close()
-#             if conn:
-#                 conn.close()
-#
-#         return result
-#
-#
-#     def insert(self, data):
-#
-#         cursor = None
-#         conn = None
-#
-#         result = []
-#
-#         try:
-#             conn = self.connection()
-#             cursor = conn.cursor()
-#             for i in data.index:
-#                 query = f"INSERT INTO dbo.doctors VALUES ('{data['first_name'][i]}', '{data['last_name'][i]}', '{data['specialty'][i]}', {data['latitude'][i]}, {data['longtitude'][i]})"
-#
-#                 cursor.execute(query)
-#                 cursor.commit()
-#
-#
-#         except Exception as e:
-#             raise e
-#         finally:
-#             if cursor:
-#                 cursor.close()
-#             if conn:
-#                 conn.close()
-#
-#         return result
+import pyodbc
+
+class SqlReader:
+    def __init__(self, config):
+        self._config = config
+        self.connstring = 'DRIVER=ODBC Driver 17 for SQL Server;SERVER={};DATABASE={};UID={};PWD={}'.format(
+            self._config['sqlserver'],
+            self._config['database'],
+            self._config['user'],
+            self._config['password'])
+
+    def connection(self):
+        connection = pyodbc.connect(self.connstring)
+        return connection
+
+    def fetch(self, specialty):
+        if specialty != 'all':
+            query = f"SELECT * FROM dbo.doctors WHERE specialty = '{specialty}'"
+        else:
+            query = "SELECT * FROM dbo.doctors"
+
+        cursor = None
+        conn = None
+
+        result = []
+
+        try:
+            conn = self.connection()
+            cursor = conn.cursor()
+            cursor.execute(query)
+
+            columns = [column[0] for column in cursor.description]
+
+            for row in cursor.fetchall():
+                result.append(dict(zip(columns, row)))
+
+        except Exception as e:
+            raise e
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                conn.close()
+
+        return result
+
+
+    def insert(self, data):
+
+        cursor = None
+        conn = None
+
+        result = []
+
+        try:
+            conn = self.connection()
+            cursor = conn.cursor()
+            for i in data.index:
+                query = f"INSERT INTO dbo.doctors VALUES ('{data['first_name'][i]}', '{data['last_name'][i]}', '{data['specialty'][i]}', {data['latitude'][i]}, {data['longtitude'][i]})"
+
+                cursor.execute(query)
+                cursor.commit()
+
+
+        except Exception as e:
+            raise e
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                conn.close()
+
+        return result
--- app.py
+++ app.py
@@ -4,23 +4,25 @@ import simplejson as json
 from SqlReader import SqlReader
 
 
-# with open('db_config.json') as config_file:
-#     conf_str = config_file.read()
-#     db_config = json.loads(conf_str)
+with open('db_config.json') as config_file:
+    conf_str = config_file.read()
+    db_config = json.loads(conf_str)
 
 app = Flask(__name__)
 # api = Api(app)
-
-reader = SqlReader(db_config)
 
 
 @app.route('/doctors/', defaults={'specialty': 'all'})
 @app.route('/doctors/<string:specialty>',methods = ['GET'])
 def doctors_all_or_by_specialty(specialty):
-    return {"results with pyodbc" : specialty}
+    try:
+        reader = SqlReader(db_config)
+        return {"results with pyodbc": specialty}
+    except Exception as e:
+        return {"exception" : str(e)}
+
     # result = reader.fetch(specialty)
     # return json.dumps(result, use_decimal=True)
-
 
 if __name__ == "__main__":
     app.run()
