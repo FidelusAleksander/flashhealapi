d8398ec749154e5dc6271503580a9acf85dd1646
Aleksander Fidelus <alek.fidelus@gmail.com>
2021-04-28 21:15:15 +0200

    DoctorMarker resource fully functional, first complete version

--- batch/resources/DoctorMarker.py
+++ batch/resources/DoctorMarker.py
@@ -7,14 +7,21 @@ from sqlalchemy.types import Float
 
 class DoctorMarkerList(Resource):
     def get(self):
-        reviews = db.session.query(DoctorReviewModel.doctor_id,
-                                   db.func.round(db.func.avg(db.func.cast(DoctorReviewModel.star_rating, Float)), 1).label('avg_review'),
-                                   db.func.count(DoctorReviewModel.star_rating).label('review_count'),
-                                   )\
-            .group_by(DoctorReviewModel.doctor_id).subquery()
-        # markers = DoctorModel.query.join(reviews, DoctorModel.doctor_id == reviews.c.doctor_id).all()
-        markers = db.session.query(DoctorModel,
-                                   reviews).outerjoin(reviews, DoctorModel.doctor_id == reviews.c.doctor_id).all()
-        for r in markers:
-            doctor_id = r[0]
-        return []
+        try:
+            reviews = db.session.query(DoctorReviewModel.doctor_id,
+                                       db.func.round(db.func.avg(db.func.cast(DoctorReviewModel.star_rating, Float)), 1).label('avg_review'),
+                                       db.func.count(DoctorReviewModel.star_rating).label('review_count'),
+                                       )\
+                .group_by(DoctorReviewModel.doctor_id).subquery()
+            results = db.session.query(DoctorModel, reviews).outerjoin(reviews, DoctorModel.doctor_id == reviews.c.doctor_id).all()
+            markers = []
+            for marker in results:
+                new_marker = marker.DoctorModel.json()
+                new_marker['avg_review'] = marker.avg_review if marker.avg_review else 0
+                new_marker['review_count'] = marker.review_count if marker.review_count else 0
+                markers.append(new_marker)
+
+            return markers, 200
+
+        except:
+            return {"message": "An error occurred while retrieving DoctorMarkers."}, 500
